2622aec3b861dc0387c562213b255df43cacd6bf
rudy <rudywork@163.com>
2022-02-11 20:51:02 +0800

    first commit

---
+++ src/main/java/com/rudywork/dao/GitTest.java
@@ -0,0 +1,13 @@
+package com.rudywork.dao;
+
+/**
+ * @author cheng
+ * @date 2022/2/11 20:49
+ * @filename GitTest
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
